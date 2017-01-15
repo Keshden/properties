4e91f313741e1420c679be69dcdc430e1926405c
Keshden <bleonrey7@gmail.com>
2017-01-15 11:10:49 -0700

    create home and about page and update layouts, add bootstrap gem and sprockets

--- db/migrate/20161221001452_users.rb
+++ db/migrate/20161221001452_users.rb
@@ -1,8 +1,9 @@
 class Users < ActiveRecord::Migration[5.0]
   def change
-    t.string :username
-    t.string :password
-    t.string :adminClearanceLevel
-    
+    create_table :users do |t|
+      t.string :username
+      t.string :password
+      t.string :adminClearanceLevel
+    end
   end
 end
