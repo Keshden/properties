dee5291c7f0f4170955f2c26aa2b8cd76d70783d
Keshden <bleonrey7@gmail.com>
2017-02-14 19:14:03 -0700

    add tasks db and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170203044431) do
+ActiveRecord::Schema.define(version: 20170215020549) do
 
   create_table "properties", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20170203044431) do
     t.float    "sqFootage"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "priority",    default: "low"
   end
 
   create_table "users", force: :cascade do |t|
